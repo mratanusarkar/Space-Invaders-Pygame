4dbe8d04c9627ca99fb7ee342d0c83a30392ed97
Atanu Sarkar <mratanusarkar@gmail.com>
2023-02-27 21:49:15 +0530

    update runner with venv + install + run main.py

--- runner.bat
+++ runner.bat
@@ -1,4 +1,2 @@
 @echo off
-pip install -r requirements.txt
-cls
-cd Space Invaders && python main.py
+cmd /k "py -m pip install --user virtualenv & py -m venv env .\env\Scripts\activate & pip install -r requirements.txt & cls & cd Space Invaders && python main.py"
